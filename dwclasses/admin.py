17a12d9f6b0e9485f6ae766af930e57ea0abdcd1
David Marks <renoc@gmx.com>
2015-06-08 18:29:08 -0400

    Daily Improvements

--- dwclasses/admin.py
+++ dwclasses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from combinedchoices.admin import (
+    BaseCCObjAdmin, ChoiceField, ChoiceFieldAdmin)
+from dwclasses.models import (
+    ClassChoice, CompendiumClass, CompletedCharacter, CombinedClass)
 
-# Register your models here.
+
+class CompendiumClassAdmin(BaseCCObjAdmin):
+    model = CompendiumClass
+
+
+admin.site.register(ClassChoice)
+admin.site.register(CompendiumClass, CompendiumClassAdmin)
+#admin.site.register(ChoiceField, ChoiceFieldAdmin)
+admin.site.register(CompletedCharacter)
+admin.site.register(CombinedClass)
--- nav/tests.py
+++ nav/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+# End-To-End test
+class Get_Tests(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Hello Dungeon World')
